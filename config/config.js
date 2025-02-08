5526e1018ca07965b0df79af609d4944fe4e2c98
Armen <matevosyan.2011@gmail.com>
2025-02-08 17:09:01 +0400

    fixed database issue

---
+++ config/config.js
@@ -0,0 +1,23 @@
+module.exports.config = {
+  "development": {
+    "username": process.env.DATABASE_USER,
+    "password": process.env.DATABASE_PASSWORD,
+    "database": process.env.DATABASE_NAME,
+    "host": process.env.DATABASE_HOST,
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": process.env.DATABASE_USER,
+    "password": process.env.DATABASE_PASSWORD,
+    "database": process.env.DATABASE_NAME,
+    "host": process.env.DATABASE_HOST,
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": process.env.DATABASE_USER,
+    "password": process.env.DATABASE_PASSWORD,
+    "database": process.env.DATABASE_NAME,
+    "host": process.env.DATABASE_HOST,
+    "dialect": "mysql"
+  }
+}
