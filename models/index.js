96a96fef7e599659982c65632eb8434be43ec4af
admin <matevosyan.2011@gmail.com>
2025-11-03 21:33:53 +0400

    aa

--- models/index.js
+++ models/index.js
@@ -2,44 +2,59 @@
 
 const fs = require('fs');
 const path = require('path');
-const Sequelize = require('sequelize');
+const { Sequelize, DataTypes } = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
-const dbUrl = new URL('mysql://root:FlufkIbwBIjVuInkmIAebzjutvwpZgWw@viaduct.proxy.rlwy.net:38787/railway');
+
+console.log('config', config);
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-   sequelize = new Sequelize(dbUrl.pathname.substring(1), dbUrl.username, dbUrl.password, {
-    host: dbUrl.hostname,
-    port: dbUrl.port,
-    dialect: "mysql",
-    logging: false, // Set to `console.log` if you want query logs
-  });}
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (
-      file.indexOf('.') !== 0 &&
-      file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
-    );
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
+if (config) {
+    // ✅ Don't use "const" again here
+    sequelize = new Sequelize(config.database, config.username, config.password, {
+        host: config.host,
+        dialect: config.dialect || 'mysql',
+        logging: false,
+        dialectOptions: {
+            connectTimeout: 60000,
+            ssl: false,
+        },
+        pool: {
+            max: 10,
+            min: 0,
+            acquire: 30000,
+            idle: 10000,
+        },
+        retry: {
+            max: 5,
+        },
+    });
+}
 
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
+// ✅ Load all models dynamically
+fs.readdirSync(__dirname)
+    .filter((file) => {
+        return (
+            file.indexOf('.') !== 0 &&
+            file !== basename &&
+            file.slice(-3) === '.js' &&
+            file.indexOf('.test.js') === -1
+        );
+    })
+    .forEach((file) => {
+        const model = require(path.join(__dirname, file))(sequelize, DataTypes);
+        db[model.name] = model;
+    });
+
+// ✅ Initialize associations
+Object.keys(db).forEach((modelName) => {
+    if (db[modelName].associate) {
+        db[modelName].associate(db);
+    }
 });
 
 db.sequelize = sequelize;
